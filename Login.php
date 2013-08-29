c3ec3082241410c711c421aeaed1bfdd8360badb
Michael Weingert <mpweinge@uwaterloo.ca>
2013-08-28 16:33:41 -1000

    Register, Login page done for now

--- Login.php
+++ Login.php
@@ -133,7 +133,7 @@ else if($_POST['submit']=='Register')
     $_POST['username'] = mysql_real_escape_string($_POST['username']);
     $con = mysqli_connect("localhost", "root", "Soccer1&", "GPSInfo");
 
-    $query = "SELECT * FROM tz_members WHERE usr='{$_POST['username']}'");
+    $query = "SELECT * FROM tz_members WHERE usr='{$_POST['username']}'";
     $row = mysqli_fetch_assoc(mysqli_query($con, $query));
 
     if ($row['usr'])
@@ -175,7 +175,7 @@ else if($_POST['submit']=='Register')
         $mailer = Swift_Mailer::newInstance($transport);
         $message = Swift_Message::newInstance('Wonderful Subject')
           ->setFrom(array('mpweingert@gmail.com' => 'Michael Weingert'))
-          ->setTo(array('mpweingpert@gmail.com' => 'Michael Weingert'))
+          ->setTo(array('mpweingert@gmail.com' => 'Michael Weingert'))
           ->setBody('This is the text of the mail send by Swift using SMTP transport.');
         //$attachment = Swift_Attachment::newInstance(file_get_contents('path/logo.png'), 'logo.png');  
         //$message->attach($attachment);
