cf502f53619066a027f0cd4bfedf428e6b9ef1aa
root <root@me@michaelweingert.com>
2013-08-31 05:57:35 -0400

    Populating list of trails with photos

--- Photos.php
+++ Photos.php
@@ -5,15 +5,14 @@ header("Content-Type: text/plain");
 function ReadGPSInformationFromDB()
 {
 	$con = mysqli_connect("localhost", "root", "Soccer1&", "GPSInfo");
-	$query = "SELECT LATDEGREE, LATMINUTE, LATSECOND, LONGDEGREE, LONGMINUTE
-	, LONGSECOND, TRAILID FROM PHOTOS";
+	$query = "SELECT * FROM PHOTOS";
 	$results = mysqli_query($con, $query);
 	if ($results)
 	{	
 		$LongData = array();
 		while ($row = $results->fetch_assoc())
 		{
-			array_push($Longdata, $row);
+			array_push($LongData, $row);
 		}
 		echo json_encode($LongData);
 	}
